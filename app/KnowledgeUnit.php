01724e15435f8b5cce1665c7e1a5a52b8d803b5c
Necip Onur Uzun <nouzun@gmail.com>
2015-12-05 19:32:51 +0200

    Added my project

---
+++ app/KnowledgeUnit.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class KnowledgeUnit extends Model
+{
+    protected $fillable = ['title'];
+
+    // Questions will be here
+}
---
+++ resources/views/pages/projects.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.sidebar')
+@section('content')
+    i am the projects page
+@stop
